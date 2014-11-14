6bf21f08d1e2699075f3a7448e7d589cb1d9bfde
Cem Olcay <cem@IT-91.local>
2014-11-14 09:24:20 +0200

    Initial Commit

---
+++ DeserializableSwiftGenerator/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DeserializableSwiftGenerator
+//
+//  Created by Cem Olcay on 14/11/14.
+//  Copyright (c) 2014 Cem Olcay. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
